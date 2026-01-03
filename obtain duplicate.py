12e6f4fe99686bbd58d4234339b2a3f44f4e88db
Adarshsoni1224 <adarshsoni1224@gmail.com>
2026-01-03 23:35:09 +0530

    duplicate values sorting

--- obtain duplicate.py
+++ obtain duplicate.py
@@ -1,19 +1,19 @@
-# nums = [1,2,3,4,5,12,5,89]
-# print(nums)
-# # nums.sort is used for sorting nums inputs!
-# nums.sort()
-# #if its dublicate is give false :
-# duplicate = False
-# # it run code form 1 to length of nums inputs:
-# for i in range(1, len(nums)):
-#     # if nums{}[i] is equal to nums[i-1] then it says true:
-#     if nums[i] == nums[i - 1]:
-#         duplicate = True
-#         break  
+nums = [1,2,3,4,5,12,5,89]
+print(nums)
+# nums.sort is used for sorting nums inputs!
+nums.sort()
+#if its dublicate is give false :
+duplicate = False
+# it run code form 1 to length of nums inputs:
+for i in range(1, len(nums)):
+    # if nums{}[i] is equal to nums[i-1] then it says true:
+    if nums[i] == nums[i - 1]:
+        duplicate = True
+        break  
 
-# print(duplicate)
-# print()
+print(duplicate)
+print()
 
 
-a=input("Enter the value":)
-if a==()
+# a=input("Enter the value":)
+# if a==()
