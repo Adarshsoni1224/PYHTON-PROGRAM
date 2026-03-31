cec2cdf9bce74fff2b7b30fa0ffcd2ec1d7677b9
Adarshsoni1224 <adarshsoni1224@gmia;l.com>
2026-03-31 20:06:17 +0530

    new

---
+++ bash1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Source and Destination files
+
+source_file="file.txt"
+destination_file="file1.txt"
+
+cp "$source_file" "$destination_file"
+
+echo "Content copied successfully!"
